28f2050b24d569930ba8ecafdc8f4b196d8f6c31
richielly <richielly.positivo@gmail.com>
2021-08-10 14:39:26 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
+if uploaded_file:
+    dados = pd.read_excel(uploaded_file)
+    dados = pd.read_excel(uploaded_file, header=None, skiprows=1)
+    dados.dropna(axis=1, how='all')
+    dados.dropna(axis=0, how='all')
+    dados.notna()
+    st.dataframe(dados)
+    st.table(dados)
+
+#     #df.loc[:, ~df.columns.str.contains('^Unnamed')]
+#     st.dataframe(df)
+#     st.table(df)
+
+
+# print(dados)
